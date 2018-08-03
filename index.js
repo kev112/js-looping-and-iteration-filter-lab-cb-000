0a3f3a5babf5ada16501ad7dc380baace9f2cdab
Tao <werewaswas@live.com>
2018-08-03 01:54:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file
+function findMatching(array, match) {
+  return array.filter(function (element) {
+    return element == match || element == match.toLowerCase()
+  })
+}
